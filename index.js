280b559fd5a6413c132dd19e3a0b1570fe8d3095
Dima <mthwhsn@gmail.com>
2022-03-28 23:08:43 +0300

    dicee

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ dice2();
 var randomNumber1 = Math.ceil(Math.random() * 6); // 1 - 6 random number 
 
 function dice1() {
-    document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`); // ./images/dice1.png - ./images/dice6.png
+    document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`); // ./images/dice1.png - ./images/dice6.png
 
 }
 
 var randomNumber2 = (Math.ceil(Math.random() * 6)) // 1 - 6 random number
 
 function dice2() {
-    document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`); // ./images/dice1.png - ./images/dice6.png
+    document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`); // ./images/dice1.png - ./images/dice6.png
     
 }
 
