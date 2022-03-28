df51d3bf0121e95f9400aca4bdd1218f2f5ca68a
Dima <mthwhsn@gmail.com>
2022-03-28 22:41:27 +0300

    Dicee v1

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.ceil(Math.random() * 6); // 1 - 6 random number 
+
+function dice1() {
+    document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`); // ./images/dice1.png - ./images/dice6.png
+
+}
+
+var randomNumber2 = (Math.ceil(Math.random() * 6)) // 1 - 6 random number
+
+function dice2() {
+    document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`); // ./images/dice1.png - ./images/dice6.png
+    
+}
+
+function wonLoseDraw() {
+    if (randomNumber1 === randomNumber2) {
+        document.querySelector("h1").innerHTML = "Draw!"; // if no one wins
+    }
+    else if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩player 1 wins!"; // if player 1 wins
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "player 2 wins!🚩"; // if player 2 wins
+    }
+}
+
+wonLoseDraw();
+dice1();
+dice2();
